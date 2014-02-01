8affbba37a1b57b2e84f6ee9818c53c84b85ee0a
Daniel M <daniel.maldonado@gmail.com>
2014-01-31 19:51:21 -0500

    Sacado el promedio de la hoja de valores de la calculadora de personas

--- src/js/app/main.js
+++ src/js/app/main.js
@@ -26,15 +26,33 @@ requirejs.config(
 
 
 
-require(['../models/PersonaCalculadora'], function(PersonaCalculadora) {
-        /*
-         $.ajax({
-         url: '/data/personas/hoja5.csv',
-         success: function(e) {
-         console.log(e.split("\r"));
-         },
-         dataType: 'text'
-         });
-         */
-        var x = new PersonaCalculadora();
+require(['jquery', '../models/PersonaCalculadora'], function($, PersonaCalculadora) {
+    var hoja3 = [];
+    var hoja3Sectores = [];
+    var promedio = [];
+    
+    var hoja3Request = $.ajax({url: '/data/personas/hoja3',dataType: 'text'});
+    var hoja3SectoresRequest = $.ajax({ url: '/data/personas/sectores_hoja3', dataType: 'text'});
+    $.when( hoja3SectoresRequest, hoja3Request ).done(function(s,d){     
+            _.each(d[0].split("\n"),function(row){
+                row = _.map(row.split("\t"),function(x){return parseFloat(x)});
+                hoja3.push(row);
+            });
+            var counter = 0;
+            _.each(s[0].split("\n"),function(row){
+                hoja3Sectores.push(row);
+                var row = [];
+                for(var y=0;y<=5;y++){ 
+                    row[y] = 0;
+                    for(var x=0;x<=3;x++){
+                        var value = parseFloat(hoja3[counter][(x*6)+y]);
+                        row[y] += parseFloat(hoja3[counter][(x*6)+y]);
+                    }
+                    row[y] = row[y]/3;
+                }
+                promedio.push(row);
+            });         
+    });
+    
+    var x = new PersonaCalculadora();
 });
