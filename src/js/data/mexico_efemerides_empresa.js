ceee26766d310a8e36f03af9d093f5e545ca8974
Daniel M <daniel.maldonado@gmail.com>
2014-02-22 20:50:29 +0100

    efemerides

---
+++ src/js/data/mexico_efemerides_empresa.js
@@ -0,0 +1,16 @@
+define(['underscore','../data/mexico_empresa_estados','../data/mexico_empresa_sectores'], function(_,estados,sectores) {
+    
+    var efemerides = {};
+    
+    _.map(sectores,function(v,i){
+        efemerides[v] = {};
+        _.map(estados,function(vv,ii){
+            efemerides[v][vv.id] = '';
+        })
+    })
+    
+    efemerides[0][19] = '¿Sabías qué Nuevo León ocupa el 1º lugar en productividad en cuanto al sector de agricultura y ganadería?';
+    
+    
+    return efemerides;
+    });
---
+++ src/js/data/mexico_efemerides_persona.js
@@ -0,0 +1,6 @@
+define([], function() {
+    
+    var efemerides = {};
+    
+    return efemerides;
+    });
