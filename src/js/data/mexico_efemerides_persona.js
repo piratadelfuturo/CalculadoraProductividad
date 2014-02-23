d648643b238a8efcf0f33695bb0f32b64a47a1a0
Rodolfo Sánchez Garza <hola@ideageek.mx>
2014-02-22 23:33:51 -0600

    Corrección de texto

--- src/js/data/mexico_efemerides_persona.js
+++ src/js/data/mexico_efemerides_persona.js
@@ -6,11 +6,11 @@ define(['underscore', '../data/mexico_persona_estudios'], function(_, estudios) 
         efemerides[i] = [];
     })
     
-    efemerides[0].push('Sabías qué el sector más productivo para una persona con primaria incompleta es el de servicios profesionales, científicos y técnicos.');
-    efemerides[1].push('Sabías qué el sector más productivo para una persona con primaria completa es el servicios de esparcimiento, culturales y deportivos, y otros servicios recreativos');
-    efemerides[2].push('Sabías qué el sector más productivo para una persona con secundaria completa es el de dirección de corporativos y empresas.');
-    efemerides[3].push('Sabías qué el sector más productivo para una persona con educación media superior y superior es el de dirección de corporativos y empresas.');
-    efemerides[4].push('Ahora sabes tu grado de productividad, para una mayor eficacia le sugerímos seleccionar un grado de estudios.');
+    efemerides[0].push('¿Sabías qué el sector más productivo para una persona con primaria incompleta es el de servicios profesionales, científicos y técnicos?');
+    efemerides[1].push('¿Sabías qué el sector más productivo para una persona con primaria completa es el servicios de esparcimiento, culturales y deportivos, y otros servicios recreativos?');
+    efemerides[2].push('¿Sabías qué el sector más productivo para una persona con secundaria completa es el de dirección de corporativos y empresas?');
+    efemerides[3].push('¿Sabías qué el sector más productivo para una persona con educación media superior y superior es el de dirección de corporativos y empresas?');
+    efemerides[4].push('Ahora sabes tu grado de productividad, para una mayor eficacia le sugerímos seleccionar un grado de estudios.?');
    
     return efemerides;
 });
